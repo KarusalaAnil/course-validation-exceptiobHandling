465ed1e3600cefb813d359321ed886bf3c647ffa
Karusala Anil <karusalaanil02@gmail.com>
2022-09-11 13:26:54 +0530

    new changes commit to stag

---
+++ src/main/java/com/springboot/CourseValidationExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseValidationExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseValidationExceptionHandlingApplication.class, args);
+	}
+
+}
